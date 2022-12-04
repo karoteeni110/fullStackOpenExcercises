5bb138d01f969d06d10a41ab2452ad16c051d891
karoteeni110 <ansaan1105@gmail.com>
2022-12-04 20:17:12 +0000

    ex 2.12 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,21 +9,22 @@ const App = () => {
     const [newQuery, setNewQuery] = useState('')
 
     // `content` and helper functions
-    const queriedData = () => {
-        return data
-            .map(country => country.name.common)
-            .filter(name => name.toLowerCase().includes(newQuery.toLowerCase()))
-    }
-    const n_results = 11 // queriedData.length
+    const queriedData = data.filter(country =>
+        country.name.common
+            .toLowerCase()
+            .includes(newQuery.toLowerCase()))
+    const n_results = queriedData.length
+    console.log(`nresults ${n_results}`);
     const overTenContent = () => [["Too many matches. Specify another filter."], false]
-    const twoToTenContent = () => {
-        return [["two to ten countries", "yes"], false]
+    const twoToTenContent = () => [queriedData.map(country => country.name.common), false]
+    const oneContent = () => {
+        console.log(`queried: ${queriedData}`)
+        return [queriedData, true]
     }
-    const oneContent = () => [[], true]
     const [content, findsOneResult] = n_results > 10 ? overTenContent()
         : n_results > 1 ? twoToTenContent()
-        : n_results === 1 ? oneContent()
-        : [[], false]
+            : n_results === 1 ? oneContent()
+                : [[], false]
 
     // Event handlers
     const handleNewQuery = (event) => {
@@ -31,12 +32,13 @@ const App = () => {
     }
 
     // Fetch
-    // useEffect(() => {
-    // axios
-    //     .get('https://restcountries.com/v4.1/all')
-    //     .then(resp => {
-    //         setData(resp.data)
-    // }, [])
+    useEffect(() => {
+        axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(resp => {
+                setData(resp.data)
+            })
+    }, [])
 
     return (
         <div>
--- part2/countries/src/components/Content.js
+++ part2/countries/src/components/Content.js
@@ -1,14 +1,16 @@
 const Content = ({content, oneResult}) => {
+    console.log(oneResult ? content[0].languages : '')
     return oneResult 
         ? (
             <div>
-                <h2>{content[0].name}</h2>
+                <h2>{content[0].name.common}</h2>
                 <p>capital {content[0].capital[0]}</p>
                 <p>area {content[0].area}</p>
                 <h3>languages:</h3>
                 <ul>
-                    {content[0].languages.map(lang => <li>lang</li>)}
+                    {Object.values(content[0].languages).map(lang => <li key={lang}>{lang}</li>)}
                 </ul>
+                <img src={content[0].flags.png} alt=""></img>
             </div>
         )
         : <div>{content.map(str => <p key={str}>{str}</p>)}</div>
